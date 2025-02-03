8d91a9774c0c45e8532c4965c175852d01c45510
Janne <tapioninetytwo@live.se>
2025-02-03 17:38:39 +0100

    Created a WPF presentation, some intitial settings like installed community.mvvm and made some basic folders to be able to make my frontend.

--- Presentation_WPF_HansAB/MainWindow.xaml.cs
+++ Presentation_WPF_HansAB/MainWindow.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System.Text;
+﻿using Presentation_WPF_HansAB.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Presentation_WPF_HansAB
+namespace Presentation_WPF_HansAB;
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow(MainViewModel mainViewModel)
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        DataContext = mainViewModel;
     }
 }
---
+++ Presentation_WPF_HansAB/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Presentation_WPF_HansAB.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public MainViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+}
