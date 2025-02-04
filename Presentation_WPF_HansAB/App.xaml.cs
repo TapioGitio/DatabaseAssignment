361cef282e0ce29f8f3c36b9b359ca99be46e49f
Janne <tapioninetytwo@live.se>
2025-02-04 08:35:09 +0100

    Made folders for the viewmodels im planning to use and registered the services i currently have in app.xaml file

--- Presentation_WPF_HansAB/App.xaml.cs
+++ Presentation_WPF_HansAB/App.xaml.cs
@@ -1,14 +1,50 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Business.Interfaces;
+using Business.Services;
+using Data.Context;
+using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace Presentation_WPF_HansAB
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+
+            .ConfigureServices(services =>
+            {
+                services.AddDbContext<DataContext>(options => options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\vsProjects\DatabaseAssignment\Data\Data\Local_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
+                services.AddScoped<IProjectRepository, ProjectRepository>();
+                services.AddScoped<IProjectManagerRepository, ProjectManagerRepository>();
+                services.AddScoped<ICustomerRepository, CustomerRepository>();
+                services.AddScoped<IStatusRepository, StatusRepository>();
+                services.AddScoped<IServiceRepository, ServiceRepository>();
+                services.AddMemoryCache();
+
+                services.AddScoped<IProjectService, ProjectService>();
+                services.AddScoped<IProjectManagerService, ProjectManagerService>();
+                services.AddScoped<ICustomerService, CustomerService>();
+                services.AddScoped<IStatusService, StatusService>();
+                services.AddScoped<IServiceService, ServiceService>();
+
+                services.AddScoped<MainWindow>();
+            })
+            .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 
 }
--- Presentation_WPF_HansAB/ViewModels/MainViewModel.cs
+++ Presentation_WPF_HansAB/ViewModels/MainViewModel.cs
@@ -1,10 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Presentation_WPF_HansAB.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
     private readonly IServiceProvider _serviceProvider;
+
+    [ObservableProperty]
+    private ObservableObject _currentViewModel = null!;
+
+    [ObservableProperty]
+    private string _headline = "Hans Mattin-Lassei AB";
+
+
+
+
+
+
 
     public MainViewModel(IServiceProvider serviceProvider)
     {
